bf1b2233618f1b9d541e084dfb4389d40fc9508e
pavelolofinskiy <pavelolofinskiy@gmail.com>
2025-10-09 20:01:28 +0300

    fixed variable parsing

--- app/View/View.php
+++ app/View/View.php
@@ -8,8 +8,10 @@ class View
     public function render($TemplatePath, $data) {
         $file = file_get_contents('app/View/' . $TemplatePath . '.php');
 
+        $output = $file;
+
         foreach ($data as $key => $value) {
-                $output = str_replace('{{ ' . $key . ' }}', $value, $file);
+                $output = str_replace('{{ ' . $key . ' }}', $value, $output);
         }
 
         echo $output;
